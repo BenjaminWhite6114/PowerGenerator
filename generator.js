6481279027453fd8601478bb70a7911530218f44
Ben White <benwhite6114@gmail.com>
2022-08-11 15:07:47 -0400

    All progress so far

--- generator.js
+++ generator.js
@@ -6,8 +6,8 @@ document.getElementById("output").innerHTML = "Javascript was loaded, but didn't
 const animals = ["any animal", "any bug", "any domesticated animals", "any aquatic animals", "any animal which can fly", "people", "cats", "dogs", "rats", "spiders", "flies", "horses", 
 "cows", "bats", "snakes", "worms", "beetles", "birds", "fish", "whales","eels", "lizards", "chickens", "possums", "raccoons", "frogs", "turtles"];
 const plants = ["any plant", "any herb", "any edible plant", "trees", "flowers", "cacti", "vines", "ferns", "grasses", "bushes", "mushrooms", "mosses"];
-const bodyParts = ["any body part", "head", "body", "arms", "legs", "hands", "feet", "eyes", "mouth", "nose", "ears", "teeth", "skin", "hair", "knuckles", "elbows", "knees", "fingers", 
-"toes", "neck"];
+const bodyParts = ["any body part", "heads", "bodys", "arms", "legs", "hands", "feet", "eyes", "mouths", "noses", "ears", "teeth", "skin", "hair", "knuckles", "elbows", "knees", "fingers", 
+"toes", "necks"];
 const materials = ["anything", "stone", "clay", "metal", "sand", "wood", "plastic", "rubber", "cement", "cotton", "cloth", "steal", "brass", "bronze", "paper", "glass", "diamond", "meat"];
 const chemicals = ["hydrogen", "helium", "carbon", "nitrogen", "oxygen", "flourine", "neon", "sodium", "magnesium", "aluminium", "silicon", "sulfur", "chlorine", "argon", "potassium", 
 "calcium", "titanium", "magnesium", "iron", "cobalt", "nickle", "copper", "zinc", "silver", "tin", "iodine", "xenon", "cesium", "tungsten", "osmium", "iridium", "platinum", "gold", 
@@ -31,8 +31,12 @@ const randomIndex = (array) =>{
 }
 //generate a random target. target(matrix of target types that are acceptable)
 const target = (type) => {
-	const targetGroup = randomIndex(type);
-	return randomIndex(targetGroup);
+	const finalTarget = {
+		target: "target property was never set"
+	}
+	finalTarget.type = randomIndex(type);
+	finalTarget.target = randomIndex(finalTarget.type);
+	return finalTarget;
 }
 //generate a random distance
 range = () => {
@@ -50,10 +54,20 @@ sense = () => {
 	const power = {
 		output: "power's output property never changed"
 	}
-	power.target = target(allTargets);
+	power.targetObject = target(allTargets);
+	power.target = power.targetObject.target;
+	power.targetType = power.targetObject.type;
 	power.range = range();
 	power.output = "You can sense " + power.target + " " + power.range;
 	return power;
 }
+imbue = () => {
+	const power = {
+		output: "power's output property never changed"
+	}
+	
+	return power;
+}
+
 //SET OUTPUT LINE OF HTML
 document.getElementById("output").innerHTML = sense().output;
